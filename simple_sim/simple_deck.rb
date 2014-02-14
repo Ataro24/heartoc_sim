f9db2992df993caad2cea38dff0bea95951bd6fe
Ataro24 <touch.taro@gmail.com>
2014-02-15 01:43:56 +0900

    add function to simple_deck

--- simple_sim/simple_deck.rb
+++ simple_sim/simple_deck.rb
@@ -16,6 +16,14 @@ class SimpleDeck
     end
   end
 
+  def reset
+    @stock.clear
+    @trash.clear
+    @hand.clear
+    configure
+  end
+
+
   #カードを1枚デッキに追加する
   def add(coin_power)
     @stock << coin_power
@@ -70,9 +78,38 @@ class SimpleDeck
     @stock.shuffle!
   end
 
+  def all_cards
+    tmp = []
+    tmp += @hand
+    tmp += @stock
+    tmp += @trash
+    tmp
+  end
+
+  def get_size
+    tmp = all_cards
+    tmp.size
+  end
+
   # 平均コイン力を出力する
   def get_coin_avg
-    @stock.inject(0.0){|r,i| r+=i }/@stock.size
+    tmp = all_cards
+    tmp.inject(0.0){ |sum, c| sum + c }/tmp.size
   end
+
+  # 手札のcoin力を出す
+  def get_hand_coin
+    @hand.inject{ |sum, c| sum + c}
+  end
+
+  def get_breakdown
+    cards = all_cards
+    farm = cards.select{|n| n == 1}.size
+    city = cards.select{|n| n == 2}.size
+    metro = cards.select{|n| n == 3}.size
+    ascend = cards.select{|n| n == 0}.size
+    return farm, city, metro, ascend
+  end
+
 
 end
